1b4e06df0d38d3f10bc9952e9849e9645b2922f1
catsoap <sjehann@gmail.com>
2019-09-29 09:21:01 +0200

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+# Road to learn React companion repo
+
+https://leanpub.com/the-road-to-learn-react
---
+++ src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  var helloWorld = 'Welcome to the Road to learn React';
+  return (
+    <div className="App">
+      <h1>{helloWorld}</h1>
+    </div>
+  );
+}
+
+export default App;
