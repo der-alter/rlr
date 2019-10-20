9f3e3502e3a3595bb32fbfc605302c18369daf78
catsoap <sjehann@gmail.com>
2019-10-20 12:26:19 +0200

    add first unidirectionnal data flow

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 
 const list = [
@@ -20,11 +20,27 @@ const list = [
   },
 ];
 
-function App() {
-  return (
-    <div className="App">
-      {list.map(function(item) {
-        return (
+class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      list,
+    };
+
+    this.onDismiss = this.onDismiss.bind(this);
+  }
+
+  onDismiss(id) {
+    const isNotId = item => item.objectID !== id;
+    const updatedList = this.state.list.filter(isNotId);
+    this.setState({ list: updatedList });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {this.state.list.map(item => (
           <div key={item.objectID}>
             <span>
               <a href={item.url}>{item.title}</a>
@@ -32,11 +48,16 @@ function App() {
             <span>{item.author}</span>
             <span>{item.num_comments}</span>
             <span>{item.points}</span>
+            <span>
+              <button onClick={() => this.onDismiss(item.objectID)} type="button">
+                Dismiss
+              </button>
+            </span>
           </div>
-        );
-      })}
-    </div>
-  );
+        ))}
+      </div>
+    );
+  }
 }
 
 export default App;
