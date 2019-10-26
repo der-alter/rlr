ee4209593020fc64ec721c9d4e43abcb8ce1cfc3
catsoap <sjehann@gmail.com>
2019-10-26 22:36:39 +0200

    styles

--- README.md
+++ README.md
@@ -11,3 +11,11 @@ https://github.com/the-road-to-learn-react/react-controlled-components-examples
 https://www.robinwieruch.de/react-component-composition
 
 https://www.robinwieruch.de/react-pass-props-to-component
+
+### CSS options
+
+https://github.com/styled-components/styled-components
+https://github.com/css-modules/css-modules
+https://www.robinwieruch.de/create-react-app-css-modules/
+https://www.robinwieruch.de/create-react-app-with-sass-support/
+https://www.robinwieruch.de/react-semantic-ui-tutorial/
--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import './App.css';
 
 const list = [
   {
@@ -50,9 +49,11 @@ class App extends Component {
     const { searchTerm, list } = this.state;
     return (
       <div className="App">
-        <Search value={searchTerm} onChange={this.onSearchChange}>
-          Search
-        </Search>
+        <div className="mb2">
+          <Search value={searchTerm} onChange={this.onSearchChange}>
+            Search
+          </Search>
+        </div>
         <Table list={list} pattern={searchTerm} onDismiss={this.onDismiss} />
       </div>
     );
@@ -67,22 +68,30 @@ const Button = ({ onClick, className = '', children }) => (
 
 const Search = ({ value, onChange, children }) => (
   <form>
-    {children} <input type="text" value={value} onChange={onChange} />
+    <span className="fw6">{children} </span>
+    <input
+      type="text"
+      value={value}
+      onChange={onChange}
+      className="input-reset ba b--black-20 pa2"
+    />
   </form>
 );
 
 const Table = ({ list, pattern, onDismiss }) => (
-  <div>
+  <div className="f6">
     {list.filter(isSearched(pattern)).map(item => (
       <div key={item.objectID}>
-        <span>
+        <span className="pa1">
           <a href={item.url}>{item.title}</a>
         </span>
-        <span>{item.author}</span>
-        <span>{item.num_comments}</span>
-        <span>{item.points}</span>
-        <span>
-          <Button onClick={() => onDismiss(item.objectID)}>Dismiss</Button>
+        <span className="pa1">{item.author}</span>
+        <span className="pa1">{item.num_comments}</span>
+        <span className="pa1">{item.points}</span>
+        <span className="pa1">
+          <Button onClick={() => onDismiss(item.objectID)} className="pa1 br2 fw6 black-70 pointer">
+            Dismiss
+          </Button>
         </span>
       </div>
     ))}
